d99ec2c194f2dbacda23fbb80bc55ca268dc98ec
lowerrider <nastykoocher@gmail.com>
2023-06-19 08:07:46 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import CreateFormPage from "./pages/CreateFormPage/CreateFormPage";
 import { ROUTES } from "./constants/constants";
@@ -8,20 +8,15 @@ import "./fonts/SBSansInterface.ttf";
 import "./fonts/SBSansInterfaceBold.ttf";
 
 import "./index.css";
-const router = createBrowserRouter([
-  {
-    path: ROUTES.index,
-    element: <MainPage />,
-  },
-  {
-    path: ROUTES.createForm,
-    element: <CreateFormPage />,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTES.index} element={<MainPage />} />
+        <Route path={ROUTES.createForm} element={<CreateFormPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
